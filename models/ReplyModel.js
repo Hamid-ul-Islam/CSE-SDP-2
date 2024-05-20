6f680847d4608e95d453c0d7e0884e140e4e7a41
Hamidul Islam <hamidthedev@gmail.com>
2024-05-20 10:44:48 +0600

    user model fix

--- models/ReplyModel.js
+++ models/ReplyModel.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 const replySchema = mongoose.Schema(
   {
     commentId: { type: String, required: true },
-    user: { type: String, required: true },
+    user: {},
     reply: { type: String, required: true },
   },
   {
